02686a6c711ec83a65efb7b2fc611ccbe8d1cb62
wjy <jianyuan.wei@hand-china.com>
2019-04-20 21:58:53 +0800

    初始化工程

---
+++ src/main/java/com/wjy/rsmapping/RsmappingApplication.java
@@ -0,0 +1,13 @@
+package com.wjy.rsmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsmappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjy/rsmapping/RsmappingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wjy.rsmapping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RsmappingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
